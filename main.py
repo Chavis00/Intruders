7497ac741f8c8e83eb1f3f8a79bb4d42b99a8965
Javier Delcourt <javier@robosophy.com>
2023-04-28 15:32:20 -0300

    Imgur image uploader added

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from src.FaceRecognition import FaceRecognition
 from src.garbage_collector import GarbageCollector
+from src.imgur_uploader import ImgurHelper
 from src.telegram_bot import TelegramBot
 from src.webcam import Webcam
-from config import CASCPATH, OUTPUTPATH, CAMERA, SIZE_PX, TOKEN, CHAT_ID, ENDPOINT, FKEY
+from config import CASCPATH, OUTPUTPATH, CAMERA, SIZE_PX, TOKEN, ENDPOINT, FKEY, INSTALLED_APPS, USERS_IDS, CLIENT_ID
+
+
+def init_applications(web_cam: Webcam) -> None:
+    if "TELEGRAM_BOT" or "AZURE_FACE_DETECTION" or "IMGUR" in INSTALLED_APPS:
+        telegram_bot = TelegramBot(users_ids=USERS_IDS, token=TOKEN, priority=0)
+        web_cam.add_observer(telegram_bot)
+    if "AZURE_FACE_DETECTION" in INSTALLED_APPS:
+        azure_face_detection = FaceRecognition(endpoint=ENDPOINT, fkey=FKEY, priority=1, telegram_bot=telegram_bot)
+        web_cam.add_observer(azure_face_detection)
+    if "GARBAGE_COLLECTOR" in INSTALLED_APPS:
+        garbage_collector = GarbageCollector(priority=2)
+        web_cam.add_observer(garbage_collector)
+    if "IMGUR" in INSTALLED_APPS:
+        imgur_helper = ImgurHelper(client_id=CLIENT_ID, telegram_bot=telegram_bot, priority=1)
+        web_cam.add_observer(imgur_helper)
 
 
 if __name__ == '__main__':
     web_cam = Webcam(CASCPATH, OUTPUTPATH, CAMERA, SIZE_PX)
-    telegram_bot = TelegramBot(chat_id=CHAT_ID, token=TOKEN, priority=1)
-    azure_face_detection = FaceRecognition(endpoint=ENDPOINT, fkey=FKEY, priority=0, telegram_bot=telegram_bot)
-    garbage_collector = GarbageCollector(priority=2)
-
-    web_cam.add_observer(azure_face_detection)
-    web_cam.add_observer(telegram_bot)
-    web_cam.add_observer(garbage_collector)
-
+    init_applications(web_cam)
     web_cam.capture()
--- src/imgur_uploader.py
+++ src/imgur_uploader.py
@@ -1,8 +1,25 @@
+import datetime
+
 from pyimgur import Imgur
-from config import CLIENT_ID
+from src import logging_config
+from src.observer import Observer
+from src.telegram_bot import TelegramBot
 
 
-def upload(PATH):    
-    im = Imgur(CLIENT_ID)
-    uploaded_image = im.upload_image(PATH, title="Chavis333")
-    return str(uploaded_image.link)
+class ImgurHelper(Observer):
+    def __init__(self, client_id: str, telegram_bot: TelegramBot, priority: int):
+        super().__init__(priority)
+        self.client = Imgur(client_id)
+        self.telegram_bot = telegram_bot
+
+    async def update(self, file_path: str, current_time=datetime.datetime):
+        now = current_time.strftime("%H:%M:%S -- %d-%m-%Y")
+        img_url = self.upload(path=file_path, description=now)
+        msg = "Image URL: "+img_url
+        if self.telegram_bot is not None:
+            await self.telegram_bot.send_msg(msg=msg)
+        logging_config.logging.info("Image uploaded to Imgur! " + img_url)
+
+    def upload(self, path: str, description: str) -> str:
+        uploaded_img = self.client.upload_image(path, title="Intruder", description=description)
+        return str(uploaded_img.link)
